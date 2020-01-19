9026b2ccea21e76a66c7ad4922e28ddb97f82ce0
xyh-cosmo <yhxu@nao.cas.cn>
2020-01-19 14:44:27 +0800

    ...

--- test_Bytes/main.cpp
+++ test_Bytes/main.cpp
@@ -31,18 +31,65 @@ int main( ){
 //    printf("2进制数值:    %s\n",s2);
 //  ###############################################################################
 
-    char s = 'a';
-    printf("s = %s\n", &s);
-    printf("s = %d\n", s);
+//    char s = 'a';
+//    printf("s = %s\n", &s);
+//    printf("s = %d\n", s);
 
-    char ss = s - 26;
-    printf("s = %s\n", &ss);
-    printf("s = %d\n", ss);
-    printf("s-26 = %s\n", (char *)(&s-26));
+//    char ss = s - 26;
+//    printf("s = %s\n", &ss);
+//    printf("s = %d\n", ss);
+////    printf("s-26 = %s\n", (char *)(&s-26));
 
-    ss = ss + 26;
-    printf("s = %s\n", &ss);
-    printf("s = %d\n", ss);
+//    ss = ss + 26;
+//    printf("s = %s\n", &ss);
+//    printf("s = %d\n", ss);
+
+//    int n = 26;
+//    short m = 26;
+
+//    printf("sizeof(s) = %ld\n", sizeof(s));
+//    printf("sizeof(n) = %ld\n", sizeof(n));
+//    printf("sizeof(m) = %ld\n", sizeof(m));
+
+// ######################################
+    char s[] = "abcdef";
+
+    printf("s0 = %s\n", &s[0]);
+    printf("s1 = %s\n", &s[1]);
+    printf("s2 = %s\n", &s[2]);
+    printf("s3 = %s\n", &s[3]);
+    printf("s4 = %s\n", &s[4]);
+
+    char ss0 = s[0]-26;
+    char ss1 = s[1]-26;
+    char ss2 = s[2]-26;
+    char ss3 = s[3]-26;
+    char ss4 = s[4]-26;
+
+    printf("ss0 = %s\n", &ss0);
+    printf("ss1 = %s\n", &ss1);
+    printf("ss2 = %s\n", &ss2);
+    printf("ss3 = %s\n", &ss3);
+    printf("ss4 = %s\n", &ss4);
+
+//    char sss4 = s[4]-26;
+//    char sss3 = s[3]-26;
+//    char sss2 = s[2]-26;
+//    char sss1 = s[1]-26;
+//    char sss0 = s[0]-26;
+
+//    printf("sss0 = %s\n", &sss0);
+//    printf("sss1 = %s\n", &sss1);
+//    printf("sss2 = %s\n", &sss2);
+//    printf("sss3 = %s\n", &sss3);
+//    printf("sss4 = %s\n", &sss4);
+
+    int a=0x1234;
+    char b=*(char*)&a;
+    if(b==0x12)
+        printf("big end\n");
+    else
+        printf("little end\n");
 
     return 0;
 }
--- test_UCHAR/main.cpp
+++ test_UCHAR/main.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
 
-    unsigned char a[4]="";
-//    int a[4];
-    a[0] = 0xfe;
-    a[1] = 0xff;
-    a[2] = 0x7f;
-    a[3] = 0x00;
+//    unsigned char a[4]="";
+////    int a[4];
+//    a[0] = 0xfe;
+//    a[1] = 0xff;
+//    a[2] = 0x7f;
+//    a[3] = 0x00;
 
-    for( int i=0; i<4; ++i ){
-        cout << "a[" << i << "] = " << int(a[i]) << endl;
-    }
+//    for( int i=0; i<4; ++i ){
+//        cout << "a[" << i << "] = " << int(a[i]) << endl;
+//    }
 
-    char32_t b = 12345555;
+//    char32_t b = 12345555;
 
-    cout << " b = " << b << endl;
+//    cout << " b = " << b << endl;
+
+
+    char s = 'a';
+
+    printf("==> Before operation:\n");
+    printf("s   = %s\n",&s);
+    printf("s   = %d\n",s);
+
+
+    char s0 = s;
+    char ss = s0-26;
+    printf("==> After operation:\n");
+    printf("ss  = %s\n",&ss);
+    printf("ss  = %d\n",s-26);
+
+    printf("s   = %s\n",&s);
+    printf("s   = %s\n",&s);
+    printf("s   = %d\n",s);
+    cout << "s  = " << s << endl;
+    cout << "s  = " << int(s) << endl;
 
     return 0;
 }
