6e01d34850b722b147960eee8a4763aee1983701
xyh-cosmo <yhxu@nao.cas.cn>
2020-02-29 15:39:00 +0800

    add TestLib

---
+++ TestLib/testlib.cpp
@@ -0,0 +1,5 @@
+#include "testlib.h"
+
+TestLib::TestLib()
+{
+}
---
+++ TestLib/testlib.h
@@ -0,0 +1,12 @@
+#ifndef TESTLIB_H
+#define TESTLIB_H
+
+#include "TestLib_global.h"
+
+class TESTLIB_EXPORT TestLib
+{
+public:
+    TestLib();
+};
+
+#endif // TESTLIB_H
